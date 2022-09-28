5be4affb313eef3b0be8c9e5ed5dc5826430276b
Steven Ruzicka <ruzickas@oregonstate.edu>
2022-09-28 09:03:34 -0700

    first commit

---
+++ public/portfolio.js
@@ -0,0 +1,19 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
